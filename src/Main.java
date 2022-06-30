af8e8ce9c7d081f63547407e6404a44e8e7488cc
Vishal Sengar <vishal.sengar@paytm.com>
2022-07-01 02:05:12 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,5 @@ public class Main {
         System.out.println(len);
         System.out.println(message1 + ". " + message2);
         System.out.println(message2.indexOf("l!"));
-        System.out.println(message1.startsWith("Hell"));
-        System.out.println(message2.endsWith("al"));
     }
 }
