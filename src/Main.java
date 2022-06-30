1d55c103d1bf408a7777ffbebf44ebda4bc1a066
Vishal Sengar <vishal.sengar@paytm.com>
2022-07-01 01:41:06 +0530

    Used endsWith() Method

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println(message1 + ". " + message2);
         System.out.println(message2.indexOf("l!"));
         System.out.println(message1.startsWith("Hell"));
+        System.out.println(message2.endsWith("al"));
     }
 }
