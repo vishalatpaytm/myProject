c2b481fbc1f45f5869c38213a64507ca937b8c42
Vishal Sengar <vishal.sengar@paytm.com>
2022-07-01 01:00:21 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String message1 = "Hello World";
+        String message2 = new String("Good Morning Vishal!");
+        int len = message1.length();
+        System.out.println(len);
+        System.out.println(message1 + ". " + message2);
+        System.out.println(message2.indexOf("l!"));
+    }
+}
